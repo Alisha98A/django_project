75e9ef0008170818b01c9f39fc68d5f799d91fac
Alisha <alisha@Alishas-MacBook-Air.local>
2024-08-20 18:01:25 +0200

    create mode 10064 Procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$mw_yc&l9j38w4!_*pf31-fko5@&c8+d^aj*imrp*68yjs^um('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
